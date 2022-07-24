ac1993048e0fb118c7b01f8ab6e91855435e68f3
DJehSantana <jessicah.dias23@hotmail.com>
2022-07-24 19:44:43 -0300

    importando fonte, separando estilos gerais do site no arquivo style.css testanto função js menu mobile

---
+++ js/header.js
@@ -0,0 +1,31 @@
+/* Criando classe para receber elementos do menu mobile*/
+class MobileNavbar {
+    constructor(mobileMenu, navList, navLinks) {
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelectorAll(navLinks);
+        this.activeClass = "active";
+
+    }
+
+    addClickEvent() {
+        this.mobileMenu.addEventListener("click", () => 
+        console.log ("Menu ativo"));
+    }
+
+    init(){
+        if (this.mobileMenu) {
+            this.addClickEvent();
+        }
+        return this;
+    }
+}
+
+const mobileNavbar = new MobileNavbar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+
+);
+
+mobileNavbar.init();
