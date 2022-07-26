5eeaf4969116b889aa46dc0a509b8ee6f28f9c25
Wellington Nobre <wellington.multilaser@gmail.com>
2022-07-26 15:12:53 -0300

    nova carousel para pagina

--- js/carousel.js
+++ js/carousel.js
@@ -1,37 +1,16 @@
-//Criando array pra receber imagens do Carrossel
-imgs = document.querySelectorAll(".carousel-images img");
+let slides = document.querySelectorAll('.slide-container');
+let index = 0;
 
-// controladores de troca de imagem
-
-let tempo = 4000;
-let imgAtiva = 0;
-const maxImg = imgs.length - 1;
-
-
-//Função que realiza a troca de imagens automaticamente
-function inicia (){       
-    setInterval(() => {
-        trocaImg(1)}, tempo
-    )
-    
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length;
+    slides[index].classList.add('active');
 }
 
-
-function trocaImg(dir){
-    tempo = 0;
-    imgs[imgAtiva].classList.remove("active");
-    imgAtiva += Number(dir);
-    if (imgAtiva > maxImg){
-        imgAtiva = 0;
-    } else if (imgAtiva < 0){
-        imgAtiva = maxImg;
-    }
-    imgs[imgAtiva].classList.add("active");
-
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length;
+    slides[index].classList.add('active');
 }
 
-//Chmando função de troca automatica de imagens 
-//quando a página é carregada
-
-
-window.addEventListener("load", inicia);
+setInterval(next, 4000);
