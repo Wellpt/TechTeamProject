9b497378978deff59bc57f6342d961f5d4f80129
DJehSantana <jessicah.dias23@hotmail.com>
2022-07-27 14:02:28 -0300

    ajustes form modal js

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+let modal = document.querySelector('.formulario-modal');
+
+function abrir() {
+    modal.style.display = 'block';
+}
+
+function fechar(){
+    modal.style.display = 'none';
+}
+
+
+
